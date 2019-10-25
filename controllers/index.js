5d6a488d753ef57cef14ef80ffa1601f6fe45aba
Michael Karr <mdkarr01@gmail.com>
2019-10-25 14:32:50 -0500

    adding cluster maps

--- controllers/index.js
+++ controllers/index.js
@@ -1,28 +1,39 @@
+// Add post model to top of file
+const Post = require('../models/post');
+// Add mapBoxToken from ENV variable
+const mapBoxToken = process.env.MAPBOX_TOKEN;
 const User = require('../models/user');
 const passport = require('passport');
 
 module.exports = {
-	// POST /register
-	async postRegister(req, res, next) {
-		const newUser = new User({
-			username: req.body.username,
-			email: req.body.email,
-			image: req.body.image
-		});
+  // GET /
+  async landingPage(req, res, next) {
+    // find all posts to populate into map
+    const posts = await Post.find({});
+    // render home page and pass in posts
+    res.render('index', { posts, mapBoxToken, title: 'Surf Shop - Home' });
+  },
+  // POST /register
+  async postRegister(req, res, next) {
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      image: req.body.image
+    });
 
-		await User.register(newUser, req.body.password);
-		res.redirect('/');
-	},
-	// POST /login
-	postLogin(req, res, next) {
-		passport.authenticate('local', {
-		  successRedirect: '/',
-		  failureRedirect: '/login' 
-		})(req, res, next);
-	},
-	// GET /logout
-	getLogout(req, res, next) {
-	  req.logout();
-	  res.redirect('/');
-	}
-}
+    await User.register(newUser, req.body.password);
+    res.redirect('/');
+  },
+  // POST /login
+  postLogin(req, res, next) {
+    passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/login'
+    })(req, res, next);
+  },
+  // GET /logout
+  getLogout(req, res, next) {
+    req.logout();
+    res.redirect('/');
+  }
+};
--- models/post.js
+++ models/post.js
@@ -4,47 +4,60 @@ const Review = require('./review');
 const mongoosePaginate = require('mongoose-paginate');
 
 const PostSchema = new Schema({
-	title: String,
-	price: String,
-	description: String,
-	images: [ { url: String, public_id: String } ],
-	location: String,
-	coordinates: Array,
-	author: {
-		type: Schema.Types.ObjectId,
-		ref: 'User'
-	},
-	reviews: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'Review'
-		}
-	],
-	avgRating: { type: Number, default: 0 }
+  title: String,
+  price: String,
+  description: String,
+  images: [{ url: String, public_id: String }],
+  location: String,
+  geometry: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  },
+  properties: {
+    description: String
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Review'
+    }
+  ],
+  avgRating: { type: Number, default: 0 }
 });
 
 PostSchema.pre('remove', async function() {
-	await Review.remove({
-		_id: {
-			$in: this.reviews
-		}
-	});
+  await Review.remove({
+    _id: {
+      $in: this.reviews
+    }
+  });
 });
 
 PostSchema.methods.calculateAvgRating = function() {
-	let ratingsTotal = 0;
-	if(this.reviews.length) {
-		this.reviews.forEach(review => {
-			ratingsTotal += review.rating;
-		});
-		this.avgRating = Math.round((ratingsTotal / this.reviews.length) * 10) / 10;
-	} else {
-		this.avgRating = ratingsTotal;
-	}
-	const floorRating = Math.floor(this.avgRating);
-	this.save();
-	return floorRating;
-}
+  let ratingsTotal = 0;
+  if (this.reviews.length) {
+    this.reviews.forEach(review => {
+      ratingsTotal += review.rating;
+    });
+    this.avgRating = Math.round((ratingsTotal / this.reviews.length) * 10) / 10;
+  } else {
+    this.avgRating = ratingsTotal;
+  }
+  const floorRating = Math.floor(this.avgRating);
+  this.save();
+  return floorRating;
+};
 
 PostSchema.plugin(mongoosePaginate);
 
